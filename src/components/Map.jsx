c031e8a7186c70356c6d0c7bf1f99aae930c0257
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-07-02 16:59:39 +0400

    add new components and hooks

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>map</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change poss
+      </button>
+    </div>
+  );
 }
 
 export default Map;
